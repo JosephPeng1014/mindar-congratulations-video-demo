bf69a8ee6e699744d543682137959ab45a3a1c88
彭鐘賢 <josephpeng1014@gmail.com>
2022-11-03 10:15:12 +0800

    update

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,59 @@
 
+// const main = async()=>{
+//   const params = new Proxy(new URLSearchParams(window.location.search), {
+//     get: (searchParams, prop) => searchParams.get(prop),
+//   });
+//   const token = params.token;
+
+//   if(token){
+//     const url = `https://d685-2001-b011-7001-3d90-811f-5a8d-5d1e-5be2.jp.ngrok.io/api/qrcode/get?token=${token}`
+    
+//     const result = await fetch(url,{
+//       mode: 'cors',
+//       method: "POST",
+//       headers: {
+//         'Access-Control-Allow-Origin':'*'
+//       }
+//     })
+//     .then(response => {
+//       return response.json()
+//     })
+//     .catch((error) => {
+//       console.log('error',error)
+//     })
+
+//     const qrcode = result?.qrcode
+//     const project = qrcode?.project
+//     const resources = project?.resources ||[]
+//     const resource = resources[0] || {}
+//     const media = resource?.media
+
+//     if(media && media.src){
+//       $("#videoFireworks").attr("src", media?.src);
+//     }
+
+//   }
+// }
+
+// http://localhost:1234/?token=custom-7kHo05F6
+
+// 用來fetch project資料
 const main = async()=>{
+  // 先清用local storage
+  localStorage.clear();
+
+
+  //從url拿到token 
   const params = new Proxy(new URLSearchParams(window.location.search), {
     get: (searchParams, prop) => searchParams.get(prop),
   });
   const token = params.token;
 
+
+
   if(token){
-    const url = `https://d685-2001-b011-7001-3d90-811f-5a8d-5d1e-5be2.jp.ngrok.io/api/qrcode/get?token=${token}`
+    // togather 平台的api位置
+    const url = `https://f553-2001-b011-7001-3890-a05d-4c01-6cd-7dc.jp.ngrok.io/api/qrcode/get?token=${token}`
     
     const result = await fetch(url,{
       mode: 'cors',
@@ -15,24 +62,50 @@ const main = async()=>{
         'Access-Control-Allow-Origin':'*'
       }
     })
-    .then(response => {
-      return response.json()
+    .then(async response => {
+      const result = await response.json()
+
+
+      const qrcode = result?.qrcode
+      const project = qrcode?.project
+      const logo =  result?.qrcode?.project?.logo
+      const resources = (project?.resources ||[]).filter(doc=>doc.type === 'ar')
+      const resource = resources[0] || {}
+      const media = resource?.media
+      const compiledMarker = (project?.resources ||[]).find(doc=>doc.type === 'marker')
+
+      // 把資料存到localstorage
+      if(logo&& logo.src){
+        localStorage.setItem('logo', logo.src);
+      }
+
+      if(project){
+        localStorage.setItem('title', project.title);
+        localStorage.setItem('body', project.body);
+      }
+
+      if(media && media.src){
+        localStorage.setItem('media',media?.src);
+      }
+
+      if(compiledMarker){
+        const marker = compiledMarker.compiledMarker
+        localStorage.setItem('marker', marker.src);
+      }
+
+      return result
+    })
+    .then(()=>{
+      // 都完成後跳轉到ar頁面
+      window.location.replace("/play.html")
     })
     .catch((error) => {
       console.log('error',error)
     })
-
-    const qrcode = result?.qrcode
-    const project = qrcode?.project
-    const resources = project?.resources ||[]
-    const resource = resources[0] || {}
-    const media = resource?.media
-
-    if(media && media.src){
-      $("#videoFireworks").attr("src", media?.src);
-    }
-
   }
+
 }
+
+
 
 main()
